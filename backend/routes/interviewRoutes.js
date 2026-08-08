c5e9bb7eef1556ace3745870705a3593b3a07fa4
Arth Patel <arthpatelarth61@gmail.com>
2026-08-08 23:52:07 +0530

    InterviewRoute Change

--- backend/routes/interviewRoutes.js
+++ backend/routes/interviewRoutes.js
@@ -1,23 +1,29 @@
 import express from "express";
-import { startInterview, submitInterviewAnswer, getInterviewHistory, getInterviewReport, getCandidateAnalytics } from "../controllers/interviewController.js";
+import {
+  startInterview,
+  submitInterviewAnswer,
+  getInterviewHistory,
+  getInterviewReport,
+  getCandidateAnalytics,
+  getOpenScheduledInterviews,
+  claimScheduledInterview
+} from "../controllers/interviewController.js";
 import { protect } from "../middleware/authMiddleware.js";
-import { getOpenScheduledInterviews, claimScheduledInterview } from "../controllers/interviewController.js";
-
 
 const router = express.Router();
 
 router.post("/start", protect, startInterview);
 
-router.post("/:interviewId/answer",protect,submitInterviewAnswer);
+router.post("/:interviewId/answer", protect, submitInterviewAnswer);
 
-router.get("/history",protect,getInterviewHistory);
+router.get("/history", protect, getInterviewHistory);
 
-router.get("/analytics",protect,getCandidateAnalytics);
-
-router.get("/:interviewId",protect,getInterviewReport);
+router.get("/analytics", protect, getCandidateAnalytics);
 
 router.get("/scheduled", protect, getOpenScheduledInterviews);
 
 router.post("/scheduled/:scheduledId/claim", protect, claimScheduledInterview);
+
+router.get("/:interviewId", protect, getInterviewReport);
 
 export default router;
